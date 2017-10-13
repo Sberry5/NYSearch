bbe2270eb8dc076a0ed697e1c2a30de034d578a3
Sberry5 <shannon.berryheller@gmail.com>
2017-10-12 20:40:25 -0500

    html/css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,28 @@
 //Document ready function to wrap all javascript
 $(document).ready(function(){
 
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "01779c7ce4234a8ab3ac8c8c29f9eeba",
+  'q': "cubs"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});	
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+
+
+//Four input boxes
+
+//
 
 });
